97937f1af084718f5747ce592748addf07579d64
lpolerecky <l.polerecky@uu.nl>
2022-10-09 21:53:50 +0200

    updated lans_paths and lans_webupdate, which got missing

---
+++ src/lans_paths.m
@@ -0,0 +1,99 @@
+% paths necessary for running Look@NanoSIMS
+
+global be_verbous
+be_verbous = 1;
+if exist('start_lans_quietly')
+    be_verbous = ~start_lans_quietly;    
+end
+
+% path to the core LANS functions
+addpath([pwd,filesep,'fnc'],'-end');
+% path to add-on functions required for post-processing (processing metafiles)
+addpath([pwd,filesep,'postprocess'],'-end');
+
+% system-specific settings; modify with care!
+if ismac
+    
+    % MAC-OS users may have a bit of trouble to get things working and look
+    % good
+    
+    % path to fig files that define the graphical user interface (GUI)
+    addpath([pwd,filesep,'figs_win'],'-end');
+
+    if be_verbous
+        fprintf(1,'Starting Look@NanoSIMS on a Mac-OS platform.\n');
+    end
+    
+    % make sure that the PATH to pdflatex, epstopdf and gs binaries are
+    % correct
+    LATEXDIR = '/Library/TeX/texbin/';
+    GSDIR = '/usr/local/bin/';
+    
+    % no need to make any changes here
+    oldpath = getenv('PATH');
+    if isempty(strfind(oldpath,GSDIR))
+        setenv('PATH', [oldpath ':' GSDIR]);
+    end
+    
+    if ~exist([GSDIR 'gs'])==2
+        if be_verbous
+            fprintf(1,'ERROR: gs not found. Please install Ghostscript to enable PDF conversion.\n');
+        end
+    end
+
+	% Command for unzipping im.zip files, including the command options. 
+    % Here I use unzip, which is by default available on MacOS systems.
+    UNZIP_COMMAND = 'unzip -q';
+    ZIP_COMMAND = 'zip -r';
+    
+    % PDF viewer (fill path to the pdf viewer on your system
+    PDF_VIEWER = 'open';
+    
+    GUI_FONTSIZE = 10;
+
+elseif isunix
+    
+    % unix/linux users have it easy
+    
+    % path to fig files that define the graphical user interface (GUI)
+    addpath([pwd,filesep,'figs'],'-end');
+
+    if be_verbous
+        fprintf(1,'Starting Look@NanoSIMS on a Unix/Linux platform.\n');
+    end
+    
+    % Command for unzipping im.zip files, including the command options. 
+    % Here I use unzip, which is normally available on a unix system.
+    UNZIP_COMMAND = 'unzip -q';    
+    ZIP_COMMAND = 'zip -r';
+    
+    % PDF viewer
+    PDF_VIEWER = 'xreader';
+        
+    GUI_FONTSIZE = 10;
+    
+elseif ispc
+    
+    % Windows users also have it relatively easy
+    
+    % path to fig files that define the graphical user interface (GUI)
+    addpath([pwd,filesep,'figs_win'],'-end');
+
+    if be_verbous
+        fprintf(1,'Starting Look@NanoSIMS on a MS-Windows platform.\n');
+    end
+    
+    % Command for unzipping im.zip files, including the command options. 
+    % Here I use 7zip, which is a freeware program available for MS Windows.
+    % Note that the full path to the program needs to be specified here,
+    % because, based on limited experience, the path to the program is not,
+    % or may not be, known within the Matlab environment.
+    UNZIP_COMMAND = '"c:\Program Files\7-Zip\7z.exe" e';
+    ZIP_COMMAND = '"c:\Program Files\7-Zip\7z.exe" a';
+
+    % PDF viewer
+    PDF_VIEWER = '"c:\Program Files\SumatraPDF\SumatraPDF.exe"';
+    
+    GUI_FONTSIZE = 8;
+    
+end
---
+++ src/lans_webupdate.m
@@ -0,0 +1,136 @@
+% Script for updating LANS from a zip file located at a given webaddress.
+% No warranty, but it should work like this: Your old LANS is backed up,
+% the latest version is downloaded and unzipped, the MATLABPATH is updated.
+
+% (c) L. Polerecky, 2018-06-07, Utrecht University
+
+LANS_webaddress = 'https://www.dropbox.com/s/adapr3pulmhlrnl/LANS-latest-src.zip?dl=1';
+LANS_zipfile = 'f1.zip';
+LANS_backup_fname = ['LANS-backup-' datestr(now,'YYYY-mm-DD_HH-MM-SS') '.zip'];
+LANS_new_fname = 'LANS-current';
+
+aa=0;
+fprintf(1,'*** This will update your Look@NanoSIMS program. ***\n');
+fprintf(1,'1. Your current LANS files will be backed up in a zip file.\n');
+fprintf(1,'2. The most recent LANS files fill be downloaded from Dropbox and unzipped in your current LANS folder.\n');
+aa=input('Do you want to continue? (Enter 1 for yes, anything else for no.)\n');
+if isempty(aa), aa=0; end
+
+if aa==1
+
+    % open and close the LANS GUI, to be sure it's closed
+    start_lans_quietly = 1;
+    lans_paths;
+    lh=LANS;
+    delete(lh);
+    clear LANS_version
+    
+    % first make a backup of the existing LANS distribution
+    curr_dir = pwd;
+    [a,b,c]=fileparts(curr_dir);
+    old_lans_folder = [b c];
+    cd(a);
+    fprintf(1,'Creating a backup of the old LANS folder %s ... ',old_lans_folder);
+    zip(LANS_backup_fname,old_lans_folder);
+    fprintf(1,'Done.\n');
+
+    % download the latest LANS src files
+    fprintf(1,'Downloading LANS-latest-src.zip from %s ... ',LANS_webaddress);
+    outf = websave(LANS_zipfile,LANS_webaddress);
+    fprintf(1,'Done.\n');
+
+    % update now
+    fprintf(1,'Attempting LANS update ... \n');
+
+    fprintf(1,'  Unzipping %s ... ', LANS_zipfile);    
+    if exist(LANS_new_fname)>0
+        % if the remnants of the previous update remain, delete them
+        rmdir(LANS_new_fname,'s');
+    end        
+    unzip(LANS_zipfile,LANS_new_fname);
+    delete(LANS_zipfile);
+    fprintf(1,'Done.\n');
+    
+    fprintf(1,'  Updating the folder structure in %s ... ',LANS_new_fname);
+    cd(LANS_new_fname)
+    if exist('src')>0
+        cd('src');
+        movefile('*','..');
+        cd('..')
+        rmdir('src');
+    end
+    fprintf(1,'Done.\n');    
+
+    % remove the old LANS paths
+    fprintf(1,'  Updating MATLAB path ...\n');
+    lans_folders = {'fnc', 'external', 'figs', 'figs_win', 'log', 'postprocess'};
+    pathCell = regexp(path, pathsep, 'split');
+    for i=1:length(lans_folders)
+        p1=[curr_dir filesep lans_folders{i}];
+        if ispc  % Windows is not case-sensitive
+          onPath = any(strcmpi(p1, pathCell));
+        else
+          onPath = any(strcmp(p1, pathCell));
+        end
+        if onPath
+            rmpath(p1);
+            fprintf(1,'  Path %s removed from MATLAB path.\n',p1);
+        end
+    end
+    fprintf(1,'  Done.\n');
+
+    % in the folder with the current version, find out the version, and
+    % rename the folder to that version    
+    lookatnanosims;
+    lh = LANS;
+    delete(lh);
+    LANS_new_folder = ['LANS-' LANS_version];
+    
+    % because LANS was run, the paths have been again updated, so remove
+    % the newly added paths.
+    curr_dir = pwd;
+    fprintf(1,'  Updating MATLAB path ...\n');
+    pathCell = regexp(path, pathsep, 'split');
+    for i=1:length(lans_folders)
+        p1=[curr_dir filesep lans_folders{i}];
+        if ispc  % Windows is not case-sensitive
+          onPath = any(strcmpi(p1, pathCell));
+        else
+          onPath = any(strcmp(p1, pathCell));
+        end
+        if onPath
+            rmpath(p1);
+            fprintf(1,'  Path %s removed from MATLAB path.\n',p1);
+        end
+    end
+    fprintf(1,'  Done.\n');
+    
+    cd('..');
+    curr_dir = LANS_new_folder;
+    if exist(LANS_new_folder)>0
+        % the folder with the same name already exists, so delete it.
+        aa=input(sprintf('Folder %s already exists.\nDo you want to replace its content with the just downloaded LANS distribution? (Enter 1 for yes, anything else for no) ',LANS_new_folder));
+        if aa==1
+            rmdir(LANS_new_folder,'s');
+            movefile(LANS_new_fname,LANS_new_folder);
+            fprintf(1,'Folder %s renamed to %s.\n',LANS_new_fname, LANS_new_folder);            
+        else
+            fprintf(1,'Folder not renamed.\n');
+            curr_dir = LANS_new_fname;
+        end
+    else
+        movefile(LANS_new_fname,LANS_new_folder,'f');
+    end
+    cd(curr_dir);
+            
+    fprintf(1,'Backup of your old Look@NanoSIMS saved in %s.\n',[a LANS_backup_fname]);
+    fprintf(1,'Latest Look@NanoSIMS installed in %s\n',pwd);
+    fprintf(1,'Type lookatnanosims to start the program.\n');
+
+    clear a b c outf pathCell lans_folders p1 onPath
+    
+else
+    fprintf(1,'Nothing done.\n');
+end
+
+clear LANS_webaddress LANS_zipfile LANS_backup_fname LANS_new_fname aa
