33eb3079ebdf3f14796e79dfe3a812b65a7991d5
Lubos Polerecky <l.polerecky@uu.nl>
2025-06-29 01:32:03 +0200

    Writing of updated documentation started, based on the old manual from 2012.

--- src/fnc/generate_latex.m
+++ src/fnc/generate_latex.m
@@ -150,7 +150,7 @@ sfac = handles.p.height/handles.p.width;
 if 0 && sfac>=1
     size_option='height=0.3\textheight';
 else
-    size_option='width=0.41\textwidth';
+    size_option='width=0.4\textwidth';
 end
 for ii=1:ceil(nf/2)
     jj=(ii-1)*2+1;
